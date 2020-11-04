e0bd7686a76b0a945d109d6e4fe483544fe2024e
luojia65 <me@luojia.cc>
2020-11-04 20:01:03 +0800

    Small fix

--- build.rs
+++ build.rs
@@ -5,12 +5,12 @@ use std::path::PathBuf;
 fn main() {
     if env::var_os("CARGO_FEATURE_RT").is_some() {
         let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-        File::create(out.join("device.x"))
+        File::create(out.join("memory-bl602.x"))
             .unwrap()
-            .write_all(include_bytes!("device.x"))
+            .write_all(include_bytes!("memory-bl602.x"))
             .unwrap();
         println!("cargo:rustc-link-search={}", out.display());
-        println!("cargo:rerun-if-changed=device.x");
+        println!("cargo:rerun-if-changed=memory-bl602.x");
     }
     println!("cargo:rerun-if-changed=build.rs");
 }
